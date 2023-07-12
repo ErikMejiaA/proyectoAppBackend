789dec252c45e3075bdc0ea7201b9622eaf96246
ErikMejiaA <131417967+ErikMejiaA@users.noreply.github.com>
2023-07-12 08:43:16 -0500

    feat :rocket: Migracion se realizo las migraciones a la base de datos.

---
+++ Infrastructure/Data/Migrations/20230712133421_InitialCreate.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "acudiente",
+                columns: table => new
+                {
+                    Acu_codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Acu_nombreCompleto = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Acu_telefono = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Acu_direccion = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_acudiente", x => x.Acu_codigo);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "consultorio",
+                columns: table => new
+                {
+                    Cons_codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cons_nombre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_consultorio", x => x.Cons_codigo);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "espesialidad",
+                columns: table => new
+                {
+                    Esp_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Esp_nombre = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_espesialidad", x => x.Esp_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "estado_cita",
+                columns: table => new
+                {
+                    Estcita_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Estcita_nombre = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_estado_cita", x => x.Estcita_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "genero",
+                columns: table => new
+                {
+                    Gen_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Gen_nombre = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Gen_abreviatura = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_genero", x => x.Gen_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tipo_documento",
+                columns: table => new
+                {
+                    Tipdoc_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Tipodoc_nombre = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tipdoc_abreviatura = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tipo_documento", x => x.Tipdoc_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "medico",
+                columns: table => new
+                {
+                    Med_nroMatriculaProfesional = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Med_nombreCompleto = table.Column<string>(type: "varchar(120)", maxLength: 120, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Med_consultorio = table.Column<int>(type: "int", nullable: false),
+                    Med_especialidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_medico", x => x.Med_nroMatriculaProfesional);
+                    table.ForeignKey(
+                        name: "FK_medico_consultorio_Med_consultorio",
+                        column: x => x.Med_consultorio,
+                        principalTable: "consultorio",
+                        principalColumn: "Cons_codigo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_medico_espesialidad_Med_especialidad",
+                        column: x => x.Med_especialidad,
+                        principalTable: "espesialidad",
+                        principalColumn: "Esp_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    Usu_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Usu_nombre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_segdo_nombre = table.Column<string>(type: "varchar(45)", maxLength: 45, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_primer_apellido_usuar = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_segdo_apellido_usuar = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_telefono = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_direccion = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_tipodoc = table.Column<int>(type: "int", nullable: false),
+                    Usu_genero = table.Column<int>(type: "int", nullable: false),
+                    Usu_acudiente = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.Usu_id);
+                    table.ForeignKey(
+                        name: "FK_usuario_acudiente_Usu_acudiente",
+                        column: x => x.Usu_acudiente,
+                        principalTable: "acudiente",
+                        principalColumn: "Acu_codigo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_usuario_genero_Usu_genero",
+                        column: x => x.Usu_genero,
+                        principalTable: "genero",
+                        principalColumn: "Gen_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_usuario_tipo_documento_Usu_tipodoc",
+                        column: x => x.Usu_tipodoc,
+                        principalTable: "tipo_documento",
+                        principalColumn: "Tipdoc_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "cita",
+                columns: table => new
+                {
+                    Cit_codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cit_fecha = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Cit_estadoCita = table.Column<int>(type: "int", nullable: false),
+                    Cit_medico = table.Column<int>(type: "int", nullable: false),
+                    Cit_datoUsuario = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cita", x => x.Cit_codigo);
+                    table.ForeignKey(
+                        name: "FK_cita_estado_cita_Cit_estadoCita",
+                        column: x => x.Cit_estadoCita,
+                        principalTable: "estado_cita",
+                        principalColumn: "Estcita_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_cita_medico_Cit_medico",
+                        column: x => x.Cit_medico,
+                        principalTable: "medico",
+                        principalColumn: "Med_nroMatriculaProfesional",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_cita_usuario_Cit_datoUsuario",
+                        column: x => x.Cit_datoUsuario,
+                        principalTable: "usuario",
+                        principalColumn: "Usu_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cita_Cit_datoUsuario",
+                table: "cita",
+                column: "Cit_datoUsuario");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cita_Cit_estadoCita",
+                table: "cita",
+                column: "Cit_estadoCita");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cita_Cit_medico",
+                table: "cita",
+                column: "Cit_medico");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_medico_Med_consultorio",
+                table: "medico",
+                column: "Med_consultorio");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_medico_Med_especialidad",
+                table: "medico",
+                column: "Med_especialidad");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_Usu_acudiente",
+                table: "usuario",
+                column: "Usu_acudiente");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_Usu_email",
+                table: "usuario",
+                column: "Usu_email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_Usu_genero",
+                table: "usuario",
+                column: "Usu_genero");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_Usu_tipodoc",
+                table: "usuario",
+                column: "Usu_tipodoc");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cita");
+
+            migrationBuilder.DropTable(
+                name: "estado_cita");
+
+            migrationBuilder.DropTable(
+                name: "medico");
+
+            migrationBuilder.DropTable(
+                name: "usuario");
+
+            migrationBuilder.DropTable(
+                name: "consultorio");
+
+            migrationBuilder.DropTable(
+                name: "espesialidad");
+
+            migrationBuilder.DropTable(
+                name: "acudiente");
+
+            migrationBuilder.DropTable(
+                name: "genero");
+
+            migrationBuilder.DropTable(
+                name: "tipo_documento");
+        }
+    }
+}
